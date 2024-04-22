a435f8d40213b2bb70d0edec4fd8772a6cc19add
Sidney Buckner <sidney.buckner2@gmail.com>
2024-04-22 13:42:26 -0400

    Setting up Problem 2 Day 5.

---
+++ 1-arrays/2-smallestWindowSorted/day5/day5.js
@@ -0,0 +1,25 @@
+/** 
+ * Link: https://github.com/vineetjohn/daily-coding-problem
+Given an array of integers that are out of order, determine the bounds of the smallest window that must be sorted
+in order for the whole array to be in sort order.
+
+Examples:
+ * input: [3,7,5,6,9]
+ * output: (1,3) Indicating the indices of the array.
+ * input: [3,5,23,4,60,8,27,80] 
+ * output: (1,6)
+*/
+//Iterate through the array
+//Placeholder for previous number/index to compare to the next in the array 
+//Placeholder for the beginning bound to note
+//Placeholder for the end bound to return 
+const sampleArray = [3,7,5,6,9];
+const sampleArray2 = [50,36,1,23,4,17,27,39,80];
+const sampleArray3 = [3,5,23,4,60,8,27,80];
+
+const smallestSortWindow = (array)=>{
+
+}
+console.log(smallestSortWindow(sampleArray));
+console.log(smallestSortWindow(sampleArray2));
+console.log(smallestSortWindow(sampleArray3));
