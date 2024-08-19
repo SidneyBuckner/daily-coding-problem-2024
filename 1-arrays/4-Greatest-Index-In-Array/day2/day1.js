fba2e49b955a5f2caca4c9cdbf5986612ed34009
Sidney Buckner <sidney.buckner2@gmail.com>
2024-08-19 08:47:58 -0400

    Problem 4 Day 1 - Setup

---
+++ 1-arrays/4-Greatest-Index-In-Array/day2/day1.js
@@ -0,0 +1,24 @@
+/**
+Greatest Index Value in an array
+Given an integer array nums, locate the largest value in the array 
+and return its index.
+
+Example:
+
+* Input: [-2,1,-3,4,-1,2,1,-5,-4],
+* Output: 8
+* Explanation: [4] is the largest integer in the array at index 3.
+*
+* Input: [34, 105, -50, 42, 14, -5, 86]
+* Output: 1
+* Explanation: [105] is the largest integer in the array at index 1.
+*
+* Input: [-5, -1, 8, -9]
+* Output: 2
+* Explanation: [8] is the largest integer in the array at index 2.
+*/
+
+const validParens = (string)=>{
+return 'IM WORKING';
+}
+console.log(validParens('()'));
